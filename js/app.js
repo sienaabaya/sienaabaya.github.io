7e691c1fd7dc99349ed9defc1fb70ba1b329d087
Angelica Siena Abaya <siena.abaya@aestrea.com>
2015-02-26 14:37:09 +0800

    add angular-animate, animated tabs using angularjs ng-show

--- js/app.js
+++ js/app.js
@@ -1,14 +1,3 @@
-//$(function(){
-   var app = angular.module('portfolio', []).config(function($interpolateProvider){
+   var app = angular.module('portfolio', ['ngAnimate']).config(function($interpolateProvider){
        $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
    });
-//   app.controller('tabController', function(){
-//      this.tab=1;
-//      this.setTab = function(selectedTab){
-//          this.tab = setTab;
-//      };
-//      this.isSet = function(isSelected){
-//          this.tab = isSelected;
-//      };
-//   });
-//});
